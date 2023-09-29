1580d702e4d98a36ffbd43a7f9a45b3feaa30e1a
syahmi1309 <143668907+syahmi1309@users.noreply.github.com>
2023-09-29 18:03:20 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+let menu = document.querySelector("#navbar");
+let nav = document.querySelector(".menu");
+
+
+
+
+
+
+menu.onclick = () =>{
+    menu.classList.toggle("fa-times")
+    nav.classList.toggle("active")
+}
+// Get the elements with class="column"
+var elements = document.getElementsByClassName("column");
+
+// Declare a loop variable
+var i;
+
+// Full-width images
+function one() {
+    for (i = 0; i < elements.length; i++) {
+    elements[i].style.msFlex = "100%";  // IE10
+    elements[i].style.flex = "100%";
+  }
+}
+
+// Two images side by side
+function two() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.msFlex = "50%";  // IE10
+    elements[i].style.flex = "50%";
+  }
+}
+
+// Four images side by side
+function four() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.msFlex = "25%";  // IE10
+    elements[i].style.flex = "25%";
+  }
+}
+
+// Add active class to the current button (highlight it)
+var header = document.getElementById("myHeader");
+var btns = header.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
